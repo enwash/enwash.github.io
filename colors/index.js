d187874288b97207587ff7748e30c66ff3885732
enwash <ericknwash@gmail.com>
2019-05-30 14:50:46 -0500

    Added undo button to colors

--- colors/index.js
+++ colors/index.js
@@ -1,5 +1,7 @@
 let reference = document.querySelector("#reference").innerHTML;
 let colors = document.querySelector("#colors");
+let steps = [];
+let undone = [];
 
 function randIn(arr) {
 	return arr[Math.floor(Math.random() * arr.length)];
@@ -48,6 +50,7 @@ function addColorPane() {
 }
 
 function replaceMe(node) {
+	steps.push(stepString());
 	node.parentNode.parentNode.removeChild(node.parentNode);
 	addColorPane();
 }
@@ -57,13 +60,14 @@ function updateColor(node) {
 }
 
 function moveLeft(node) {
+	steps.push(stepString());
 	if (node.previousSibling != null) {
 		node.parentNode.insertBefore(node, node.previousSibling);
 	}
 }
 
 function moveRight(node) {
-	console.log("right");
+	steps.push(stepString());
 	if (node.nextSibling != null) {
 		console.log("not null");
 		node.parentNode.insertBefore(node, node.nextSibling.nextSibling);
@@ -92,32 +96,52 @@ function exportCSS() {
 	alert("Copied CSS to clipboard");
 }
 
-function exportPerma() {
-	ret = "https://query.design/colors/#";
+function stepString() {
+	ret = "";
 	cols = document.querySelectorAll(".colorLabel");
 	for (var i = 1; i < cols.length; i++) {
 		ret += cols[i].innerHTML.substr(1);
 	}
+	return ret;
+}
+
+function exportPerma() {
+	ret = "https://query.design/colors/#" + stepString();
 	copyClip(ret);
 	alert('Copied link to clipboard');
 }
-
-url = new URL(window.location.href);
 
 for (var i = 0; i < 5; i++) {
 	addColorPane();
 }
 
-if (window.location.href.indexOf('#') > 0) {
-	cls = window.location.href.split('#')[1];
-	colors.childNodes[1].childNodes[7].innerHTML = '#' + cls.substr(0,6);
-	colors.childNodes[2].childNodes[7].innerHTML = '#' + cls.substr(6,6);
-	colors.childNodes[3].childNodes[7].innerHTML = '#' + cls.substr(12,6);
-	colors.childNodes[4].childNodes[7].innerHTML = '#' + cls.substr(18,6);
-	colors.childNodes[5].childNodes[7].innerHTML = '#' + cls.substr(24,6);
+function loadStep(str) {
+	colors.childNodes[1].childNodes[7].innerHTML = '#' + str.toString().substr(0,6);
+	colors.childNodes[2].childNodes[7].innerHTML = '#' + str.toString().substr(6,6);
+	colors.childNodes[3].childNodes[7].innerHTML = '#' + str.toString().substr(12,6);
+	colors.childNodes[4].childNodes[7].innerHTML = '#' + str.toString().substr(18,6);
+	colors.childNodes[5].childNodes[7].innerHTML = '#' + str.toString().substr(24,6);
 	colors.childNodes[1].childNodes[7].dispatchEvent(new Event('focusout'));
 	colors.childNodes[2].childNodes[7].dispatchEvent(new Event('focusout'));
 	colors.childNodes[3].childNodes[7].dispatchEvent(new Event('focusout'));
 	colors.childNodes[4].childNodes[7].dispatchEvent(new Event('focusout'));
 	colors.childNodes[5].childNodes[7].dispatchEvent(new Event('focusout'));
+}
+
+function undo() {
+	undone.push(steps[steps.length - 1]);
+	loadStep(steps[steps.length - 1]);
+	console.log(undone);
+	steps.pop();
+}
+
+function redo() {
+	loadStep(undone[undone.length - 1]);
+	steps.push(stepString());
+	undone.pop();
+}
+
+if (window.location.href.indexOf('#') > 0) {
+	cls = window.location.href.split('#')[1];
+	loadStep(cls);
 }
